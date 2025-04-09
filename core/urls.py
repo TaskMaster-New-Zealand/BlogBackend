177d1548ea7b710168b2ba1e6d86b342033aa508
alanriba <arivera@ideasimple.cl>
2025-04-09 13:45:31 +1200

    Initial commit: update setting urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,10 +25,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
     
-    # Server static files in development
+# Server static files in development
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.conf import settings
-
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
